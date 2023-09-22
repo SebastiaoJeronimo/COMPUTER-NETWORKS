abd4ce613d21c39b7c28c4267c77e31d746d4ed2
Sebastiao <sc.jeronimo@campus.fct.unl.pt>
2023-09-22 23:56:56 +0100

    lab 02 assigment 2 almost done just need to correct the errors related to the invalid client number

--- lab02/Assignment2/TCPclient2.py
+++ lab02/Assignment2/TCPclient2.py
@@ -13,16 +13,27 @@ def main():
     clientSocket = socket(AF_INET,SOCK_STREAM)       # create TCP socket
     clientSocket.connect((serverName, serverPort))   # open TCP connection
 
-    number_in_string = input("Input interger: ")   # take input Integer between 1 and 100
-    name = input("please input a random name")     # asks for a name in input
-    message = name+":"+number_in_string            # transform number in string
-    
+    client_number = input("Input interger: ")             # take input Integer between 1 and 100
+    client_name = input("please input a random name: ")   # asks for a name in input
+    message = client_name+":"+client_number               # transform number in string
     
     clientSocket.send(message.encode())             # send user's sentence
-                                                 # over TCP connection
+                                                    # over TCP connection
 
-    modifiedSentence = clientSocket.recv(sockBuffer).decode()  # receive response
-    print("Message received: ", modifiedSentence)
+    server_sentence = clientSocket.recv(sockBuffer).decode()  # receive response
+    print("Message received: ", server_sentence)              # server sentence
+
+    array = server_sentence.split(":") #split the server sentence 
+    server_name = array[0]
+    server_number = array[1]
+    sum_numbers = array[2]
+
+    print("Server Name: ",server_name) # print the name of the server
+    print("Client Name: ",client_name) # print the client name
+
+    print("client number :" ,client_number) # print the client number
+    print("server number :", server_number) # print the server number
+    print("sum of both numbers :", sum_numbers) # print sum of both numbers
     
     clientSocket.close()            # close TCP connection
 
--- lab02/Assignment2/TCPserver2.py
+++ lab02/Assignment2/TCPserver2.py
@@ -4,6 +4,7 @@
 @author: cpm
 """
 from socket import *
+from random import *
 
 serverPort = 12000
 sockBuffer = 2048
@@ -16,24 +17,44 @@ def main():
     serverSocket.listen(1)              # begin listening for incoming TCP requests
     print("Server is running")
 
+    server_number = randint(1, 120) # get random number i chosed 120 to get some random numbers that exceed 100
+    print(server_number)                  # print for DEBUG
+    server_name = "Jhon the server"       # random name 
+    print(server_name)                    # print for DEBUG
+
     while True:
         connSocket, addr = serverSocket.accept()    # waits for incoming requests:
                                                     # new socket created on return
         print("Connected by: ", str(addr))
 
-        sentence = connSocket.recv(sockBuffer).decode()     # read a sentence of bytes
-                                                            # received from client
-  
-        print("Data from connected user: ", sentence)       # display received sentence
-
-
-        if (number < 1 or number > 100):          #checks if the number is valid
-            print("invalid number please put a number between 1 and 100")
-            return
+        client_sentence = connSocket.recv(sockBuffer).decode()     # read a sentence of bytes
+                                                                   # received from client
         
-        sentenceUpper = sentence.upper()                    # convert to upper case
-    
-        connSocket.send(sentenceUpper.encode())             # send modified sentence over
+        # print("Data from connected user: ", sentence)       # display received sentence
+
+        array = client_sentence.split(":")                # split the sentence by the separator :
+        client_number_in_str = array[1]            # get the number of the client
+        client_number = int(client_number_in_str)  # transform the number into an int
+        client_name = array[0]                     # get the client name
+        sum_of_numbers = client_number + server_number
+
+        print("number of client :", client_number)                    # display number of client
+        print("number of server :", server_number)                    # display number of client
+        print("sum of both numbers :",sum_of_numbers)                 # display the sum of both numbers
+        
+        print("name of client :", client_name)        # display name of client
+        print("name of server :", server_name)        # display name of server
+
+
+        if (client_number < 1 or client_number > 100):          #checks if the number is valid
+            print("invalid number please put a number between 1 and 100")
+            #maybe send something to the client to tell them the number is invalid
+            connSocket.close()  # close the socket
+            break               # exits the loop
+        
+        #create server sentence
+        server_sentence = server_name+":"+str(server_number)+":"+str(sum_of_numbers)     
+        connSocket.send(server_sentence.encode())           # send modified sentence over
                                                             # TCP connection
 
         connSocket.close()      # close TCP connection:
